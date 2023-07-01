395afb44a509403542e3013d493d9e164a208211
TranDatk <hgooshvhd123@gmail.com>
2023-07-01 15:35:08 +0700

    Model + database

--- app/src/main/java/com/nhom22/findhostel/MainActivity.java
+++ app/src/main/java/com/nhom22/findhostel/MainActivity.java
@@ -2,13 +2,22 @@ package com.nhom22.findhostel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.nhom22.findhostel.data.DatabaseHelper;
 
+
+public class MainActivity extends AppCompatActivity {
+    private DatabaseHelper databaseHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseHelper = new DatabaseHelper(this);
+        SQLiteDatabase db = databaseHelper.getWritableDatabase();
     }
+
+
 }
