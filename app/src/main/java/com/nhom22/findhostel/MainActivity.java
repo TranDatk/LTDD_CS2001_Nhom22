d977c05b2ace02f099c2e727b39fb2a466cae815
TranDatk <hgooshvhd123@gmail.com>
2023-07-04 15:47:59 +0700

    Database

--- app/src/main/java/com/nhom22/findhostel/MainActivity.java
+++ app/src/main/java/com/nhom22/findhostel/MainActivity.java
@@ -2,6 +2,7 @@ package com.nhom22.findhostel;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.UserManager;
@@ -10,7 +11,11 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+
+import com.nhom22.findhostel.data.UserAccountDAO;
 import com.nhom22.findhostel.databinding.ActivityMainBinding;
+import com.nhom22.findhostel.model.Address;
+import com.nhom22.findhostel.model.UserAccount;
 import com.nhom22.findhostel.ui.Account.AccountPageFragment;
 import com.nhom22.findhostel.ui.Extension.ExtensionPageFragment;
 import com.nhom22.findhostel.ui.Home.HomePageFragment;
@@ -18,20 +23,23 @@ import com.nhom22.findhostel.ui.Save.SavePageFragment;
 import com.nhom22.findhostel.ui.Search.SearchPageFragment;
 import com.nhom22.findhostel.data.DatabaseHelper;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLException;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-
-
         binding.navigation.setOnItemSelectedListener(item -> {
             Fragment selectedFragment = null;
             if (item.getItemId() == R.id.navigation_home) {
@@ -58,8 +66,16 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the initial selected fragment
         binding.navigation.setSelectedItemId(R.id.navigation_search);
+        databaseHelper = new DatabaseHelper(this);
+        SQLiteDatabase db = databaseHelper.getWritableDatabase();
 
+        Address address = new Address(1, "ABC", 1 , null , null , null, null);
+        UserAccount userAccount = new UserAccount(2, "trandatk2", "hgoos@gmail.com",
+                "123", "092020", 2000, 1, null,1,address);
+
+
+        UserAccountDAO dao = new UserAccountDAO(this);
+        dao.addUserAccount(userAccount);
     }
-
 
 }
--- app/src/main/java/com/nhom22/findhostel/model/Address.java
+++ app/src/main/java/com/nhom22/findhostel/model/Address.java
@@ -1,4 +1,77 @@
 package com.nhom22.findhostel.model;
 
 public class Address {
+    private int id;
+    private String houseNumber;
+    private int isActive;
+    private Cities cities;
+    private Districts districts;
+    private SubDistrics subDistrics;
+    private Streets streets;
+
+    public Address(int id, String houseNumber, int isActive, Cities cities, Districts districts, SubDistrics subDistrics, Streets streets) {
+        this.id = id;
+        this.houseNumber = houseNumber;
+        this.isActive = isActive;
+        this.cities = cities;
+        this.districts = districts;
+        this.subDistrics = subDistrics;
+        this.streets = streets;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public int getIsActive() {
+        return isActive;
+    }
+
+    public Cities getCities() {
+        return cities;
+    }
+
+    public Districts getDistricts() {
+        return districts;
+    }
+
+    public SubDistrics getSubDistrics() {
+        return subDistrics;
+    }
+
+    public Streets getStreets() {
+        return streets;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public void setIsActive(int isActive) {
+        this.isActive = isActive;
+    }
+
+    public void setCities(Cities cities) {
+        this.cities = cities;
+    }
+
+    public void setDistricts(Districts districts) {
+        this.districts = districts;
+    }
+
+    public void setSubDistrics(SubDistrics subDistrics) {
+        this.subDistrics = subDistrics;
+    }
+
+    public void setStreets(Streets streets) {
+        this.streets = streets;
+    }
 }
