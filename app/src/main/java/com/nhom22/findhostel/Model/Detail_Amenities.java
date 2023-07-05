56dceb2709e4b7e1de392f338d2a3ec389c296cb
TranDatk <hgooshvhd123@gmail.com>
2023-07-05 09:34:10 +0700

    Update Detail_Amenities.java

--- app/src/main/java/com/nhom22/findhostel/Model/Detail_Amenities.java
+++ app/src/main/java/com/nhom22/findhostel/Model/Detail_Amenities.java
@@ -2,5 +2,46 @@ package com.nhom22.findhostel.Model;
 
 public class Detail_Amenities {
     private int id;
+    private int quantity;
+    private Posts posts;
+    private Amenities amenities;
 
+    public Detail_Amenities(int id, int quantity, Posts posts, Amenities amenities) {
+        this.id = id;
+        this.quantity = quantity;
+        this.posts = posts;
+        this.amenities = amenities;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Posts getPosts() {
+        return posts;
+    }
+
+    public void setPosts(Posts posts) {
+        this.posts = posts;
+    }
+
+    public Amenities getAmenities() {
+        return amenities;
+    }
+
+    public void setAmenities(Amenities amenities) {
+        this.amenities = amenities;
+    }
 }
