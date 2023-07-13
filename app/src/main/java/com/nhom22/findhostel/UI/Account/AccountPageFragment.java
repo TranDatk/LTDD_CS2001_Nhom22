d1d40a2e37df61871ba898da9209db4a93ccbc90
Penz7 <tandatkt002@gmail.com>
2023-07-13 17:31:24 +0700

    chuyển trang

--- app/src/main/java/com/nhom22/findhostel/UI/Account/AccountPageFragment.java
+++ app/src/main/java/com/nhom22/findhostel/UI/Account/AccountPageFragment.java
@@ -2,12 +2,14 @@ package com.nhom22.findhostel.UI.Account;
 
 import static android.content.Context.MODE_PRIVATE;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
 import android.os.Bundle;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -22,6 +24,7 @@ import com.bumptech.glide.Glide;
 import com.nhom22.findhostel.Model.UserAccount;
 import com.nhom22.findhostel.R;
 import com.nhom22.findhostel.Service.UserAccountService;
+import com.nhom22.findhostel.UI.HomeSecond.HomeSecondActivity;
 import com.nhom22.findhostel.databinding.FragmentAccountPageBinding;
 
 
@@ -46,7 +49,6 @@ public class AccountPageFragment extends Fragment {
                     byte[] image = userAccount.getImage();
                     binding.screenLogined.setVisibility(View.VISIBLE);
                     binding.btnLogin.setVisibility(View.INVISIBLE);
-                    binding.btnRegister.setVisibility(View.INVISIBLE);
                     binding.userName.setText("Xin chào " + username);
                     Bitmap bitmap = BitmapFactory.decodeByteArray(image, 0, image.length);
                     binding.userAvatar.setImageBitmap(bitmap);
@@ -58,21 +60,21 @@ public class AccountPageFragment extends Fragment {
             } else {
                 binding.screenNotLogin.setVisibility(View.VISIBLE);
                 binding.btnLogin.setVisibility(View.VISIBLE);
-                binding.btnRegister.setVisibility(View.VISIBLE);
                 binding.btnLogout.setVisibility(View.INVISIBLE);
                 binding.screenLogined.setVisibility(View.GONE);
             }
 
+        binding.changeMode.setOnClickListener(v -> {
+            Intent intent = new Intent(requireActivity(), HomeSecondActivity.class);
+            startActivity(intent);
+            requireActivity().finish(); // Optional: Finish the current activity if needed
+        });
 
         binding.btnLogin.setOnClickListener(v -> {
             // Replace the fragment with LoginFragment
             replaceFragment(new LoginFragment());
         });
 
-        binding.btnRegister.setOnClickListener(v -> {
-            // Replace the fragment with RegistrationFragment
-            replaceFragment(new RegistrationFragment());
-        });
 
         binding.btnLogout.setOnClickListener(view1 -> {
             clearUserSession();
